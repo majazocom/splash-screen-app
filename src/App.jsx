7c869e995dedc5a5a931156e128e8a6d57c74228
Maja Gedda <maja.gedda@zocom.se>
2023-04-04 12:00:21 +0200

    app.jsx base

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import Splash from './pages/Splash'
+import Start from './pages/Start'
+import Products from './pages/Products'
+import Details from './pages/Details'
 
 function App() {
 
   return (
     <Router>
-      <Routes>
-        <Route path='/splash' />
-        <Route path='/start' />
-        <Route path='/products' />
-        <Route path='/details' />
-      </Routes>
+      <header>
+        <h1>Vår SPA</h1>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/splash' element={<Splash />} />
+          <Route path='/start' element={<Start />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/details' element={<Details />} />
+          <Route path='/' element={<Navigate to='/splash' />} />
+        </Routes>
+      </main>
     </Router>
   )
 }
