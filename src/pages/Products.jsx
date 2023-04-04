3f952d178f543dade0f528879ea5f1c7b316fbbb
Maja Gedda <maja.gedda@zocom.se>
2023-04-04 13:00:11 +0200

    added products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,36 @@
 function Products() {
+    const products = [
+        {
+            title: "The eye of the world",
+            author: "Robert Jordan",
+            pages: 782,
+            genre: "fantasy"
+         },
+         {
+            title: "The fellowship of the ring",
+            author: "JRR Tolkien",
+            pages: 423,
+            genre: "fantasy"
+         },
+         {
+            title: "The trial",
+            author: "Franz Kafka",
+            pages: 152,
+            genre: "philosophical fiction"
+         },
+         {
+            title: "The girl with the dragon tattoo",
+            author: "Stieg Larsson",
+            pages: 544,
+            genre: "crime"
+         },
+         {
+            title: "Of mice and men",
+            author: "John Steinbeck",
+            pages: 107,
+            genre: "tragedy"
+         }
+    ]
     return (
         <>
             <h2>PRODUCTS</h2>
